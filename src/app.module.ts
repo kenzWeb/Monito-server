47ed514ea99a420c80a8c9cbe7b103185246036a
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2023-12-08 22:43:48 +0300

    bug fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { DogModule } from './dog/dog.module';
 import { LargeDogModule } from './large-dog/largeDog.module';
 import { PopularDogModule } from './popular-dog/popularDog.module';
+import { ProductsModule } from './products/products.module';
 import { SmallDogModule } from './small-dog/smallDog.module';
 
 @Module({
-  imports: [PopularDogModule, SmallDogModule, LargeDogModule, DogModule],
+  imports: [
+    PopularDogModule,
+    SmallDogModule,
+    LargeDogModule,
+    DogModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
