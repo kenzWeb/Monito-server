e97d96908d750a8a1a0d26da5be014e1030c3966
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2023-12-08 22:19:38 +0300

    BD bug fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DogModule } from './dog/dog.module';
 import { LargeDogModule } from './large-dog/largeDog.module';
 import { PopularDogModule } from './popular-dog/popularDog.module';
 import { SmallDogModule } from './small-dog/smallDog.module';
 
 @Module({
-  imports: [PopularDogModule, SmallDogModule, LargeDogModule],
+  imports: [PopularDogModule, SmallDogModule, LargeDogModule, DogModule],
 })
 export class AppModule {}
